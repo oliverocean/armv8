b12159693ccaa25df5a41dd465fd9c58a89d4b1a
Oliver Ocean <github@oliverocean.co>
2020-07-01 16:36:47 -0500

    updates

--- src/a41a/a41a.s
+++ src/a41a/a41a.s
@@ -1,31 +1,56 @@
 /* *
  * @author: Oliver Ocean <github@oliverocean.co>
  * @file: a41a.s
+ * @brief: port the C program 'a41a' (~/data/a41a.c) to assembly language
  */
 
 /* -----------[ Text ]----------- */
+// reserved for executable instructions
             .text
             .global main
-
 main:
-            ins x0, x1
+            // load memory address of variables
+	    ldr x0, f_address
+	    ldr x1, g_address
+	    ldr x2, i_address
+	    ldr x3, j_address
 
-            // initialize variables
+            // load values from memory addresses
+	    ldr w0, [x0]
+	    ldr w1, [x1]
+	    ldr w2, [x2]
+	    ldr w3, [x3]
 
+while_loop:
+	    // while j != i   // test equality of j and i, if equal, exit loop
+	    // f = g + j      // add g and j, store in f
+	    // j++            // add j and 1, store in j
+	    // end of loop    // reevaluate, top of loop 
 
-	    // if i != j, exit loop
-	    // f = g + j
-	    // j++
-	    // re evaluate 
+print_result:
+	    ??? x8, #??		// print syscall
+	    svc #0
 
-	    // print statement
-
-  
+exit:
+	    mov x8, #93		// sys_exit
+	    svc #0
 
 /* -----------[ Data ]----------- */
+// reserved for initialized and uninitialized (read/write) data
             .data
 
-val_f: 	    .word    0x00    // i = 0
-val_g: 	    .word    0x01    // i = 1
-val_i: 	    .word    0x0A    // i = 10
-val_j: 	    .word    0x05    // i = 5
+// small values may be set as 
+// .word data type (32bits, ie: W0, W1, W2...)
+f_value:    .word    0x00    // i = 0
+g_value:    .word    0x01    // i = 1
+i_value:    .word    0x0A    // i = 10
+j_value:    .word    0x05    // i = 5
+
+// addresses need to be set as 
+// .dword data type (64-bits, ie: X0, X1, X2...) 
+f_address:  .dword   f_value
+g_address:  .dword   g_value
+i_address:  .dword   i_value
+j_address:  .dword   j_value
+
+// EOF
