3080dd731a8a62915e8ec67643f9f75891862a49
Oliver Ocean <github@oliverocean.co>
2020-07-03 11:31:21 -0500

    revised documentation

--- src/a41a/a41a.s
+++ src/a41a/a41a.s
@@ -32,7 +32,7 @@ while_loop:
 
 print_results:
 	    mov x8, #64       // sys_write from <uninstd.h>, fs/read_write.c (Linux)
-	    svc #0
+	    svc #0	      // extraneous?
 	    adr x0, outstr
 	    bl printf
 	    b exit
