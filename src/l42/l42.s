0e76afe7522c87defba9056f3717253200642103
Oliver Ocean <github@oliverocean.co>
2020-07-04 15:21:29 -0500

    modified to use stack

--- src/l42/l42.s
+++ src/l42/l42.s
@@ -5,6 +5,11 @@
  *     Modify the assembly program 'l42_origin.s' (~/data/l42.s) to 'pass' the
  *     values from main() to write() using the stack. The add operation in write()
  *     should use the values of x1, x2 as defined in the main subroutine.
+ * @note:
+ *     ARMv8 requires full 16-bytes per register; stp/ldp may be used for pairs.
+ *     Otherwise use str/ldr and manually move the stack pointer (sp) by 
+ *     16-byte increments if your data doesn't fit neatly into a pair.
+ *        
  */
 
 /* -----------[ Text ]----------- */
@@ -13,13 +18,23 @@
 main:
 	    mov x1, 1
 	    mov x2, 2
+
+	    sub sp, sp, 32
+	    str x1, [sp, 16]
+	    str x2, [sp, 0]
+	    // stp x1, x2, [sp, -16]! // alt: 'store pair' (push) onto stack (note "!")
+
 	    bl write
 	    bl exit
 
 write:
-            mov x1, #10
-            mov x2, #20
+	    ldr x1, [sp, 16]
+	    ldr x2, [sp, 0]
+	    add sp, sp, 32
+            // ldp x1, x2, [sp], 16  // alt: 'load pair' (pop) from stack
+
 	    add x3, x1, x2
+
 	    ldr x0, =msg
 	    bl printf
 
