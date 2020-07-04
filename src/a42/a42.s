690fbed0fe8bac6b0bcc0edd92db4be3edb7ac94
Oliver Ocean <github@oliverocean.co>
2020-07-04 11:56:21 -0500

    updated documentation

--- src/a42/a42.s
+++ src/a42/a42.s
@@ -9,29 +9,25 @@
             .global main
 main:
 	    mov x0, #0
-	    mov x1, #31   // c in main(), a in f3()
-	    mov x2, #128  // p in main(), b in f3()
+	    mov x1, #31       // c in main(), a in f3()
+	    mov x2, #128      // p in main(), b in f3()
 	    bl f3_func
 	    b exit
 
 f3_func:
-	    lsr x3, x1, #2 // c = a >> 2 (=7)
-	    add x2, x1, x2 // *b = a + *b
+	    lsr x3, x1, #2    // c = a >> 2 (=7)
+	    add x2, x1, x2    // *b = a + *b
 	    b if_sub
 
 if_sub:
-	    // eval: a < 2 
-	    cmp x1, #2
+	    cmp x1, #2        // eval: a < 2 
 	    blt output_results
 
-	    // eval: c < 0
-	    cmp x3, xzr
+	    cmp x3, xzr       // eval: c < 0
 	    blt output_results
 
-	    // c = c | a
-	    orr x3, x3, x1
+	    orr x3, x3, x1    // c = c | a
 	    b output_results
-
 
 output_results:
 	    adr x0, result_msg
