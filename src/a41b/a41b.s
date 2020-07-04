86f1116e340f3b7af82b2688bc1a0874805e27ee
Oliver Ocean <github@oliverocean.co>
2020-07-04 16:16:04 -0500

    wip: array access bug

--- src/a41b/a41b.s
+++ src/a41b/a41b.s
@@ -8,28 +8,27 @@
             .text
             .global main
 main:
-            // set stdout to terminal
-	    mov x0, #0
-	    mov x1, #0        // k = 0 (for_loop)
-	    mov x2, array_max_length
-	    adr x3, a_array
+	    mov x0, 0
+	    mov x1, 0         // k = 0 (for_loop)
+	    mov x2, max
+	    adr x3, array     // &array
 	    mov x4, #2        // m = 2
-
 	    bl for_loop
 	    b exit
 
 for_loop:
-            cmp x1, #5        // is k < 5 ?
-	    bge output_results // branch if so (ge: greater than or equal to)
+            cmp x1, x2        // eval: k < max
+	    bge output_results
 
 	    add x4, x4, x1    // m = m + k
 
-	    // store address for a[k] in x5
-	    lsl x5, x1, #8    // x5 = k * 8 (offset)
-	    add x5, x3, x5    // x5 = a[] + offset
-	
-	    // set a[k] = 1
-	    mov x5, #1
+	    //lsl x5, x1, #8    // x5 = k * 8 (offset)
+	    //add x5, x3, x5    // x5 = a[k] (a[] + offset)
+	    //mov x5, #1        // set a[k] = 1
+
+	    add x5, x3, x1, lsl 3
+	    str x3, [x5]
+	    add x3, x3, #1
 
             // increment index and loop
 	    add x1, x1, #1    // k++
@@ -45,12 +44,10 @@ exit:
 
 /* -----------[ Data ]----------- */
             .data
-
-a_array:    .dword   0x00, 0x00, 0x00,0x00, 0x00 // initialize with zeros
-            .set     array_max_length, 5
-
-// printf output format
-result_msg: .asciz   "> results:\n [ %d ] (k)\n [ %d ] (max)\n [ %d ] (a_array)\n [ %d ] (m)\n"
+array:      .dword   0x00, 0x00, 0x00,0x00, 0x00 // initialize with zeros
+            .set     max, 5
+result_msg: .asciz   "> result: [ %d ]\n"
+//result_msg: .asciz   "> results:\n [ %d ] (k)\n [ %d ] (max)\n [ %d ] (a_array)\n [ %d ] (m)\n"
 
 
 // EOF
