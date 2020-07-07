b7a4a545f2c2d14d455ae6453f9d76114a00d87c
Oliver Ocean <github@oliverocean.co>
2020-07-07 17:11:11 -0500

    wip: printf bug

--- src/a41b/a41b.s
+++ src/a41b/a41b.s
@@ -9,45 +9,81 @@
             .global main
 main:
 	    mov x0, 0
-	    mov x1, 0         // k = 0 (for_loop)
-	    mov x2, max
-	    adr x3, array     // &array
-	    mov x4, #2        // m = 2
+	    adr x1, array         // array base
+	    mov x2, 0             // index k = 0
+	    mov x3, max           // array length
+	    mov x4, xzr           // a[k] = 0
+	    mov x5, 1             // value to populate into array
+	    mov x6, 2             // m = 2
+
 	    bl for_loop
-	    b exit
 
 for_loop:
-            cmp x1, x2        // eval: k < max
+	    cmp x2, x3            // eval: k < max
 	    bge output_results
 
-	    add x4, x4, x1    // m = m + k
+	    add x6, x6, x2        // m = m + k
 
-	    //lsl x5, x1, #8    // x5 = k * 8 (offset)
-	    //add x5, x3, x5    // x5 = a[k] (a[] + offset)
-	    //mov x5, #1        // set a[k] = 1
+	    add x4, x1, x2, lsl 3  // x4 = x1 + (x2 * 8) : base + (index * offset)
+	    str x5, [x4]
 
-	    add x5, x3, x1, lsl 3
-	    str x3, [x5]
-	    add x3, x3, #1
-
-            // increment index and loop
-	    add x1, x1, #1    // k++
+	    add x2, x2, 1         // increment index : k++
 	    b for_loop
 
 output_results:
+            // make room on stack for 5 items (5 * 16 = 80bytes)
+	    sub sp, sp, 80
+	    
+	    // copy values onto stack
+	    ldr x7, [x1, 48] 
+	    str x7, [sp, 64]
+
+	    ldr x7, [x1, 40] 
+	    str x7, [sp, 48]
+
+	    ldr x7, [x1, 32] 
+	    str x7, [sp, 48]
+
+	    ldr x7, [x1, 24] 
+	    str x7, [sp, 32]
+
+	    ldr x7, [x1, 16] 
+	    str x7, [sp, 16]
+
+	    ldr x7, [x1, 8] 
+	    str x7, [sp, 0]
+
+	    // zero out x1 to x5
+	    mov x1, xzr
+	    mov x2, xzr
+	    mov x3, xzr
+	    mov x4, xzr
+	    mov x5, xzr
+
+	    // copy values from stack into x1 to x5
+	    ldr x5, [sp, 64]
+	    ldr x4, [sp, 48]
+	    ldr x3, [sp, 32]
+	    ldr x2, [sp, 16]
+            ldr x1, [sp, 0]
+
+	    // reset stack pointer
+	    add sp, sp, 80
+
 	    adr x0, result_msg
 	    bl printf
+	    b exit
 
 exit:
-	    mov x8, #93	      // sys_exit from <unistd.h>, kernel/exit.c (Linux)
-	    svc #0
+	    mov x8, 93	          // sys_exit from <unistd.h>, kernel/exit.c (Linux)
+	    svc 0
 
 /* -----------[ Data ]----------- */
             .data
-array:      .dword   0, 0, 0, 0, 0 // initialize with zeros for debugging
+array:      .dword   0x0e, 0x0e, 0x0e, 0x0e, 0x0e // initialize with 0x0e for debugging
             .set     max, 5
-result_msg: .asciz   "> result: [ %d ]\n"
-//result_msg: .asciz   "> results:\n [ %d ] (k)\n [ %d ] (max)\n [ %d ] (a_array)\n [ %d ] (m)\n"
+//result_msg: .asciz   "> result: [ %d ]\n"
+result_msg: .asciz   "> array a[5]: [ %d, %d, %d, %d, %d ]\n> int m: [ %d ]\n"
 
 
 // EOF
