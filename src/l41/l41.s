5c3f292a22092b6e5202e21690aac1c8ddf873a8
Oliver Ocean <github@oliverocean.co>
2020-07-07 15:04:10 -0500

    completed requirements

--- src/l41/l41.s
+++ src/l41/l41.s
@@ -8,61 +8,53 @@
             .text
             .global main
 main:
-            bl get_input
-	    bl print_input
-
-            // need to preserve lr on the stack for get_max and nested calls
-	    // ok to save lr to register for get_input because no nested calls
+	    bl get_input        
+	    mov x2, 4            // a
+	    mov x3, 3            // b
+	    mov x4, x19          // c from get_input results (x19)
 	    bl get_max
-
-	    bl print_max
-	    	
 	    b exit
 
 get_input:
-            mov x27, lr       // preserve link register (x30) when entering function
+            mov x27, lr          // preserve link register (x30) when entering function
 	    
-	    mov x0, xzr       // initialize for printf
-            adr x0, input_msg // put the address into x0 for printf
-	    bl printf         // printf input_msg
+            adr x0, input_msg    // set x0 with address for printf to write
+	    bl printf            // invoke printf (input_msg)
 
-	    //mov x1, xzr       // initialize result
-	    sub sp, sp, 48    // make room on stack (ARMv8 stack alignment restriction is 16-bytes/register)
-	    str xzr,[sp]      // clean up memory contents where input will be received
-	    mov x1, sp        // set x1 with address for scanf to read
+	    sub sp, sp, 16       // make room on stack (ARMv8 stack alignment restriction is 16-bytes/register)
+	    str xzr,[sp]         // clean up memory contents where input will be received
 	    adr x0, input_format // set x0 with address of input_format
-	    bl scanf          // store user input on stack
-	    str x2, [sp, 0]
-	    str x3, [sp, 16]
-	    str x4, [sp, 32]
-	    add sp, sp, 48    // reset stack pointer
-	    //mov x19, x1       // copy value into x19 to retain after function call
+	    mov x1, sp           // set x1 with address for scanf to read
+	    bl scanf             // invoke scanf (scans input, puts into x1, which points to sp, thus put into stack)
+	    ldr x1, [sp]         // load contents of sp into x1
+	    add sp, sp, 16       // reset stack pointer
+	    mov x19, x1          // copy x1 into x19 to preserve
 
-	    adr x0, output_msg
-	    bl printf
+	    adr x0, output_msg   // set x0 with address for printf to write
+	    bl printf            // invoke printf (output_msg)
 
-	    mov lr, x27       // restore link register (x30) before returning function
-	    ret               // branch to link register (x30); equivalent to 'br lr'
+	    mov lr, x27          // restore link register (x30) before returning function
+	    ret                  // branch to link register (x30); equivalent to 'br lr'
 
 get_max:
-            mov x1, x2        // result = a
-	    cmp x3, x1        // eval :b > result
-	    bgt b_greater     // branch if true
-	    cmp x4, x1        // eval: c > result
-	    bgt c_greater     // branch if true
-	    b a_greater       // a is greater
+            mov x1, x2           // result = a
+	    cmp x3, x1           // eval :b > result
+	    bgt b_greater        // branch if true
+	    cmp x4, x1           // eval: c > result
+	    bgt c_greater        // branch if true
+	    b a_greater          // a is greater
 
 a_greater:
 	    b output_results
 
 b_greater:
-            cmp x4, x3       // eval: c > b 
-	    bgt c_greater    // branch if true
-	    mov x1, x3       // else result = b
+            cmp x4, x3           // eval: c > b 
+	    bgt c_greater        // branch if true
+	    mov x1, x3           // else result = b
 	    b output_results
 
 c_greater:
-            mov x1, x4       // result = c
+            mov x1, x4           // result = c
 	    b output_results
 
 output_results:
@@ -70,15 +62,15 @@ output_results:
 	    bl printf
 
 exit:
-	    mov x8, #93	      // sys_exit from <unistd.h>, kernel/exit.c (Linux)
-	    svc #0
+	    mov x8, 93	         // sys_exit from <unistd.h>, kernel/exit.c (Linux)
+	    svc 0
 
 /* -----------[ Data ]----------- */
               .data
-input_msg:    .asciz "> Enter a number:\n"
+input_msg:    .asciz "> Enter a number:  "
 input_format: .string "%d"
-output_msg:   .asciz "> You entered: [ %d ]\n"
-result_msg:   .asciz "> The maximum number is: [ %d ]\n"
+output_msg:   .asciz "> The maximum of [ %d ], [ 4 ], [ 3 ] is:\n"
+result_msg:   .asciz "> [ %d ]\n"
 
 /* -----------[ EOF ]----------- */
               .end
